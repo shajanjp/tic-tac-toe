1beb9347db3716eb491b9d57268c7e532c8548c6
shajanjp <shajanjp@gmail.com>
2020-04-09 08:26:35 +0530

    fix: removes dummy data and changes return type

--- libs/game-logic.js
+++ libs/game-logic.js
@@ -1,21 +1,19 @@
-let columnData = [1,1,1,1,0,1,0,1,null]
-let player = 1;
-
-checkIfWon(player, columnData)
 function checkIfWon(player, columnData){
-  let winner = '';
-  const winArray = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
+  const winArray = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
+  const gameOver = columnData.includes(null);
+
   let isWin = winArray.some(arr =>  {
     return arr.every(prop => {
       return (columnData[prop] === player)
     })
   });
-  const gameOver = columnData.includes(null);
- return (isWin, !gameOver);
-  
 
+  return {
+    isWin, 
+    isGameOver: !gameOver
+  };
 }
 
 module.exports = {
-  checkIfGameOver
+  checkIfWon
 }
